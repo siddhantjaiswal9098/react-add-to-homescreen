c098b000c0eab179f09d2625fbc96b0bf3c2bf4e
Siddhant Jaiswal <40851531+siddhantjaiswal9098@users.noreply.github.com>
2020-05-20 20:00:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,42 @@
 import './style.scss';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { MdClose } from 'react-icons/md';
 
 import { isIos, isInStandaloneMode } from './utils';
 
-class AddToHomescreen extends Component {
-  state = {
-    bannerVisible: true,
+const AddToHomescreen = (props) => {
+  const [bannerVisible, setBannerVisible] = useState(true);
+
+  const onAddToHomescreenClick = () => {
+    props.onAddToHomescreenClick();
   };
 
-  onAddToHomescreenClick = () => {
-    this.props.onAddToHomescreenClick();
-  };
+  const handleCloseBannerBtnClick = () => setBannerVisible(false);
 
-  handleCloseBannerBtnClick = () => this.setState({ bannerVisible: false });
+  const { title, icon } = props;
+  const shouldRender = bannerVisible && isIos() && !isInStandaloneMode();
 
-  render() {
-    const { title, icon } = this.props;
-    const { bannerVisible } = this.state;
-    const shouldRender = bannerVisible && isIos() && !isInStandaloneMode();
-
-    return (
-      <div>
-        {shouldRender ? (
-          <div className="add-to-home-banner">
-            <div className="add-to-home-content">
-              {icon ? <img className="add-to-home-icon" src={icon} /> : null}
-              <div className="add-to-home-text">
-                <a onClick={this.onAddToHomescreenClick}>
-                  {title || 'Add to home screen'}
-                </a>
-              </div>
+  return (
+    <div>
+      {shouldRender ? (
+        <div className="add-to-home-banner">
+          <div className="add-to-home-content">
+            {icon ? <img className="add-to-home-icon" src={icon} /> : null}
+            <div className="add-to-home-text">
+              <a onClick={onAddToHomescreenClick}>
+                {title || 'Add to home screen'}
+              </a>
             </div>
-            <MdClose
-              className="add-to-home-close-btn"
-              onClick={this.handleCloseBannerBtnClick}
-            />
           </div>
-        ) : null}
-      </div>
-    );
-  }
+          <MdClose
+            className="add-to-home-close-btn"
+            onClick={handleCloseBannerBtnClick}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 }
+
 
 export default AddToHomescreen;
